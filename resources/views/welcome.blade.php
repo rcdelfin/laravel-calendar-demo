abdf7f5e88c63392ebe5d6ac8a011954daf0e75e
Raymund C. Delfin <rcdexe@gmail.com>
2019-10-13 18:29:28 +0800

    Update css source

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+        <link href="{{ '/css/app.css' }}" rel="stylesheet"/>
     </head>
     <body>
         <div id="app">
